6ce5721c8b87f817e50f4ffa08decc5b60ce710d
troy <troy@3e6b72d3-b629-0410-ad1b-3b4ab856adc4>
2006-09-16 08:10:01 +0000

    Pre-0.6 changes to docs and package info files.
    git-svn-id: http://ibpy.googlecode.com/svn/trunk@120 3e6b72d3-b629-0410-ad1b-3b4ab856adc4

--- ibpy/setup.py
+++ ibpy/setup.py
@@ -40,7 +40,7 @@ doclines = __doc__.split('\n')
 
 setup(
     name = 'IbPy',
-    version = '0.5',
+    version = '0.6',
     description = doclines[0],
     author = 'Troy Melhase',
     author_email = 'troy@gci.net',
