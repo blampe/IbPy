da2b462c3b62d88f26adced6e7b726f501362107
troy <troy@3e6b72d3-b629-0410-ad1b-3b4ab856adc4>
2007-02-17 05:30:26 +0000

    Updated src/Makefile to set tws_api_version number in ib package.
    Set svn:keywords on ib/__init__.py to complete the version data
    available at runtime.
    
    
    
    git-svn-id: http://ibpy.googlecode.com/svn/branches/ast@172 3e6b72d3-b629-0410-ad1b-3b4ab856adc4

---
+++ ib/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" ib -> root package for ibpy.
+
+"""
+tws_api_version = "9.00"
+ibpy_version = "0.7.%s" % (tws_api_version, )
+ibpy_revision = "$Revision$"
