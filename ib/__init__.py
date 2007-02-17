42502f4727c24d0d93f9d8325dbb6ded0fd0e89d
troy <troy@3e6b72d3-b629-0410-ad1b-3b4ab856adc4>
2007-02-17 05:37:30 +0000

    Better version and revision names and values.
    git-svn-id: http://ibpy.googlecode.com/svn/branches/ast@173 3e6b72d3-b629-0410-ad1b-3b4ab856adc4

--- ib/__init__.py
+++ ib/__init__.py
@@ -4,5 +4,5 @@
 
 """
 tws_api_version = "9.00"
-ibpy_version = "0.7.%s" % (tws_api_version, )
-ibpy_revision = "$Revision$"
+version = "0.7.%s" % (tws_api_version, )
+revision = "$Revision$".split()[1]
