bfa32933d61d1f7245c88948b8792c148b498551
troy.melhase <troy.melhase@3e6b72d3-b629-0410-ad1b-3b4ab856adc4>
2007-02-21 00:46:06 +0000

    Something amiss.
    git-svn-id: http://ibpy.googlecode.com/svn/trunk@212 3e6b72d3-b629-0410-ad1b-3b4ab856adc4

--- ib/ext/ComboLeg.py
+++ ib/ext/ComboLeg.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 ##
-## Source file: "ComboLeg.java"
-## Target file: "ComboLeg.py"
-##
-## Original file copyright original author(s).
-## This file copyright Troy Melhase <troy@gci.net>.
+#
+# Source file: "ComboLeg.java"
+# Target file: "ComboLeg.py"
+#
+# Original file copyright original author(s).
+# This file copyright Troy Melhase <troy@gci.net>.
+#
 ##
 
 from ib.aux.overloading import overloaded
