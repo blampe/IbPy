597763ef39019eb39d9efcce170a6f17237aa031
troy.melhase <troy.melhase@3e6b72d3-b629-0410-ad1b-3b4ab856adc4>
2008-05-13 06:53:39 +0000

    Continued build out.
    git-svn-id: http://ibpy.googlecode.com/svn/trunk@302 3e6b72d3-b629-0410-ad1b-3b4ab856adc4

--- ib/sym/__init__.py
+++ ib/sym/__init__.py
@@ -46,13 +46,40 @@ class HDDateFormat:
     short = 2 # 1/1/1970 GMT
 
 
-class UseRTH:
-    no, yes = 0, 1
+class YesNo:
+    no = false = 0
+    yes = true = 1
 
+
+class RTH(YesNo):
+    pass
+
+
+class AllOrNone(YesNo):
+    pass
+
+class Override(YesNo):
+    pass
+
+class FirmQuoteOnly(YesNo):
+    pass
+
+class ETradeOnly(YesNo):
+    pass
+
+class ContinuousUpdate(YesNo):
+    pass
+
+
+class AuctionStrategy:
+    match = 1
+    improvement = 2
+    transparent = 3
 
 class ServerLogLevel:
     system, error, warning, information, detail = \
             sys, err, warn, info, det = range(1, 6)
+
 
 class FaDataType:
     groups, profile, account_aliases = range(1, 4)
@@ -62,5 +89,75 @@ class ExerciseAction:
     exercise, lapse = range(1, 3)
 
 
-class Override:
-    no, yes, = 0, 1
+class TriggerMethod:
+    default = 0
+    double_askbid = 1
+    last = 2
+    double_last = 3
+
+
+class ShortSaleSlot:
+    unapplicable = 0
+    clearing_broker = 1
+    third_party = 2
+
+
+class OcaType:
+    cancel_on_fill_block = 1
+    reduce_on_fill_block = 2
+    reduce_on_fill_noblock = 3
+
+
+class Rule80a:
+    individual = 'I'
+    agency = 'A'
+    agent_other_member = 'W'
+    individual_ptia = 'J'
+    agency_ptia = 'U'
+    agent_other_member_ptia = 'M'
+    individual_pt = 'K'
+    agency_pt = 'Y'
+    agent_other_member_pt = 'N'
+
+
+class RefPriceType:
+    avg = 1
+    bidask = 2
+
+class VolatilityType:
+    daily = 1
+    annual = 2
+
+
+class GenericTickTypes:
+    option_volume = 100
+    option_open_interest = 101
+    historical_volatility = 104
+    option_implied_volatility = 106
+    index_future_premium = 162
+    misc_stats = 165
+    mark_price = 221
+    auction_values = 225
+    shortable = 236
+
+
+class TickValues:
+    low_13_week = 15
+    high_13_week = 16
+    low_26_week = 17
+    high_26_week = 18
+    low_52_week = 19
+    high_52_week = 20
+    avg_volume = 21
+    option_historical_vol = 23
+    option_implied_vol = 24
+    option_call_open_interest = 27
+    option_put_open_interest = 28
+    option_call_volume = 29
+    option_put_volume = 30
+    index_future_premium = 31
+    auction_volume = 34
+    auction_price = 35
+    auction_imbalance = 36
+    mark_price = 37
+    shortable = 46
